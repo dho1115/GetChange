acbbf5111b4e9285f1f02ac7bdbd18f6e0041a99
admin_dho <davidhorph@hotmail.com>
2025-01-27 17:06:12 -0600

    Inside AvailableCurrencies.py: I placed all of my Available Currencies into a DICTIONARY (AvailableCurrencyDict). Now, All Currencies will automatically UPDATE whenever I add a new currency inside the AvailableCurrencyDict. NOTE: Maybe in the future, I do NOT want to use all my currencies, whic is why I keep AvailableCurrencyDict separate from AllCurrencies.

--- folder/AvailableCurrencies.py
+++ folder/AvailableCurrencies.py
@@ -4,12 +4,16 @@ AvailableCurrencies.py will show all the available currencies your currency entr
 
 from functions import MoneyChanger;
 
-TwentyDollar = MoneyChanger('20 Dollar Bill', None, None)
-TenDollar = MoneyChanger('10 Dollar Bill', '20 Dollar Bill', 2)
-FiveDollar = MoneyChanger('5 Dollar Bill', '10 Dollar Bill', 2)
-OneDollar = MoneyChanger('1 Dollar Bill', '5 Dollar Bill', 5)
-Quarter = MoneyChanger('Quarter', '1 Dollar Bill', 4)
-Nickel = MoneyChanger('Nickel', 'Quarter', 5)
-Penny = MoneyChanger('Penny', 'Nickel', 5)
+# Add any NEW MonecyChanger() functions to AvailableCurrencyDict (below):
 
-AllCurrencies = [TwentyDollar, TenDollar, FiveDollar, OneDollar, Quarter, Nickel, Penny];
+AvailableCurrencyDict = dict(
+   TwentyDollar = MoneyChanger('20 Dollar Bill', None, None),
+   TenDollar = MoneyChanger('10 Dollar Bill', '20 Dollar Bill', 2),
+   FiveDollar = MoneyChanger('5 Dollar Bill', '10 Dollar Bill', 2),
+   OneDollar = MoneyChanger('1 Dollar Bill', '5 Dollar Bill', 5),
+   Quarter = MoneyChanger('Quarter', '1 Dollar Bill', 4),
+   Nickel = MoneyChanger('Nickel', 'Quarter', 5),
+   Penny = MoneyChanger('Penny', 'Nickel', 5)
+) #Changed this to a dict so I don't have to Add new AvailableCurrencyDict then Add it AGAIN to AllCurrencies (below).
+
+AllCurrencies = AvailableCurrencyDict.values();
